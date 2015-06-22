12935c007100caebcf67e2833a936152ce9971a0
azuajeaj <azuajeaj@gmail.com>
2015-06-22 16:30:12 -0400

    added fare calc

--- server.js
+++ server.js
@@ -2,17 +2,41 @@ var express = require('express'),
 	exphbs = require('express3-handlebars'),
 	app = express();
 
+	var bodyParser = require('body-parser');
+
 app.engine('handlebars', exphbs({defaultLayout: "main"}));
 
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req,res){
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-	res.render("index");
+
+
+app.get('/', function(req,res){
+	
+	res.render('index');
+
+});
+
+app.get('/about', function(req,res){
+	res.render('about')
+});
+
+app.post('/', function(req,res){
+	console.log("here");
+	console.log(req.body.balance);
+	var balance = req.body.balance;
+	var numRides= Math.floor(balance/(2.75));
+	res.render('balance', {
+		balance:balance,
+		rides: numRides
+	});
+	
 });
 
 app.use('/public', express.static('public'));   
 
-var port = Number(process.env.PORT||3000);
+var port = Number(process.env.PORT||3000);  
 
 app.listen(port);
