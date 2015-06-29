8bcc0cb05b547d00478a3732e9011ce1bf80b85d
azuajeaj <azuajeaj@gmail.com>
2015-06-29 14:13:55 -0400

    added redirection to home page

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/', function(req,res){
 	res.render('index');
 
 });
+app.get('/index', function(req,res){
+
+	res.render('index');
+
+});
 
 app.get('/about', function(req,res){
 	res.render('about')
